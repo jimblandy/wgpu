3989965c2549b67274903a10c7f09e97be6c5e4e
Jim Blandy <jimb@red-bean.com>
2022-08-14 16:54:06 -0700

    unit test for mips bounds check

--- wgpu/tests/texture_bounds.rs
+++ wgpu/tests/texture_bounds.rs
@@ -89,6 +89,49 @@ fn bad_copy_origin() {
     );
 }
 
+#[test]
+fn bad_mip_level() {
+    fn try_level(mip_level: u32, should_panic: bool) {
+        let mut parameters = TestParameters::default();
+        if should_panic {
+            parameters = parameters.failure();
+        }
+
+        initialize_test(parameters, |ctx| {
+            let texture = ctx.device.create_texture(&TEXTURE_DESCRIPTOR);
+            let buffer = ctx.device.create_buffer(&BUFFER_DESCRIPTOR);
+
+            let mut encoder = ctx
+                .device
+                .create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
+            encoder.copy_texture_to_buffer(
+                wgpu::ImageCopyTexture {
+                    texture: &texture,
+                    mip_level,
+                    origin: wgpu::Origin3d { x: 0, y: 0, z: 0 },
+                    aspect: wgpu::TextureAspect::All,
+                },
+                wgpu::ImageCopyBuffer {
+                    buffer: &buffer,
+                    layout: BUFFER_COPY_LAYOUT,
+                },
+                wgpu::Extent3d {
+                    width: 1,
+                    height: 1,
+                    depth_or_array_layers: 1,
+                },
+            );
+        });
+    }
+
+    try_level(0, false);
+    try_level(2, false);
+    try_level(3, true);
+
+    try_level(u32::MAX - 1, true);
+    try_level(u32::MAX, true);
+}
+
 const TEXTURE_SIZE: wgpu::Extent3d = wgpu::Extent3d {
     width: 64,
     height: 64,
@@ -96,9 +139,9 @@ const TEXTURE_SIZE: wgpu::Extent3d = wgpu::Extent3d {
 };
 
 const TEXTURE_DESCRIPTOR: wgpu::TextureDescriptor = wgpu::TextureDescriptor {
-    label: Some("CopyOrigin"),
+    label: Some("texture_bounds.rs texture"),
     size: TEXTURE_SIZE,
-    mip_level_count: 1,
+    mip_level_count: 3,
     sample_count: 1,
     dimension: wgpu::TextureDimension::D2,
     format: wgpu::TextureFormat::Rgba8UnormSrgb,
@@ -113,4 +156,11 @@ const BUFFER_COPY_LAYOUT: wgpu::ImageDataLayout = wgpu::ImageDataLayout {
     offset: 0,
     bytes_per_row: NonZeroU32::new(TEXTURE_SIZE.width * BYTES_PER_PIXEL),
     rows_per_image: None,
+};
+
+const BUFFER_DESCRIPTOR: wgpu::BufferDescriptor = wgpu::BufferDescriptor {
+    label: Some("texture_bounds.rs buffer"),
+    size: BUFFER_SIZE as wgt::BufferAddress,
+    usage: wgpu::BufferUsages::COPY_SRC.union(wgpu::BufferUsages::COPY_DST),
+    mapped_at_creation: false,
 };
