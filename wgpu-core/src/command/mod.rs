4640c32227df280e9d3a4bf15ce46b289f0f1c71
Erich Gubler <erichdongubler@gmail.com>
2024-11-15 11:33:44 -0500

    fixup! move `CommandEncoderStatus` on the `CommandBuffer` and change its variants to hold `CommandBufferMutable`

--- wgpu-core/src/command/mod.rs
+++ wgpu-core/src/command/mod.rs
@@ -169,7 +169,7 @@ impl CommandEncoderStatus {
 
     /// Unlocks the [`CommandBuffer`] and puts it back into the [`Self::Recording`] state.
     ///
-    /// This function is the counterpart to [`Self::lock_encoder`].
+    /// This function is the unlocking counterpart to [`Self::lock_encoder`].
     ///
     /// It is only valid to call this function if the encoder is in the [`Self::Locked`] state.
     fn unlock_encoder(&mut self) -> Result<EncoderGuard<'_>, CommandEncoderError> {
