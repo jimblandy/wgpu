87758b658f1c89df3904d4b5da568a948535df40
teoxoy <28601907+teoxoy@users.noreply.github.com>
2025-10-07 13:13:32 +0200

    add `TemplateArgsStart` & `TemplateArgsEnd` tokens

--- naga/src/front/wgsl/parse/mod.rs
+++ naga/src/front/wgsl/parse/mod.rs
@@ -702,11 +702,11 @@ impl Parser {
 
         // parse component type if present
         match (lexer.peek().0, partial) {
-            (Token::Paren('<'), ast::ConstructorType::PartialVector { size }) => {
+            (Token::TemplateArgsStart, ast::ConstructorType::PartialVector { size }) => {
                 let (ty, ty_span) = self.singular_generic(lexer, ctx)?;
                 Ok(Some(ast::ConstructorType::Vector { size, ty, ty_span }))
             }
-            (Token::Paren('<'), ast::ConstructorType::PartialMatrix { columns, rows }) => {
+            (Token::TemplateArgsStart, ast::ConstructorType::PartialMatrix { columns, rows }) => {
                 let (ty, ty_span) = self.singular_generic(lexer, ctx)?;
                 Ok(Some(ast::ConstructorType::Matrix {
                     columns,
@@ -715,8 +715,8 @@ impl Parser {
                     ty_span,
                 }))
             }
-            (Token::Paren('<'), ast::ConstructorType::PartialArray) => {
-                lexer.expect_generic_paren('<')?;
+            (Token::TemplateArgsStart, ast::ConstructorType::PartialArray) => {
+                lexer.expect(Token::TemplateArgsStart)?;
                 let base = self.type_specifier(lexer, ctx)?;
                 let size = if lexer.end_of_generic_arguments() {
                     let expr = self.const_generic_expression(lexer, ctx)?;
@@ -725,7 +725,7 @@ impl Parser {
                 } else {
                     ast::ArraySize::Dynamic
                 };
-                lexer.expect_generic_paren('>')?;
+                lexer.expect(Token::TemplateArgsEnd)?;
 
                 Ok(Some(ast::ConstructorType::Array { base, size }))
             }
@@ -1308,7 +1308,7 @@ impl Parser {
         self.push_rule_span(Rule::VariableDecl, lexer);
         let mut space = crate::AddressSpace::Handle;
 
-        if lexer.next_if(Token::Paren('<')) {
+        if lexer.next_if(Token::TemplateArgsStart) {
             let (class_str, span) = lexer.next_ident_with_span()?;
             space = match class_str {
                 "storage" => {
@@ -1322,7 +1322,7 @@ impl Parser {
                 }
                 _ => conv::map_address_space(class_str, span, &lexer.enable_extensions)?,
             };
-            lexer.expect(Token::Paren('>'))?;
+            lexer.expect(Token::TemplateArgsEnd)?;
         }
         let (name, ty) = self.optionally_typed_ident(lexer, ctx)?;
 
@@ -1424,12 +1424,12 @@ impl Parser {
         lexer: &mut Lexer<'a>,
         ctx: &mut ExpressionContext<'a, '_, '_>,
     ) -> Result<'a, (Handle<ast::Type<'a>>, Span)> {
-        lexer.expect_generic_paren('<')?;
+        lexer.expect(Token::TemplateArgsStart)?;
         let start = lexer.start_byte_offset();
         let ty = self.type_specifier(lexer, ctx)?;
         let span = lexer.span_from(start);
         lexer.next_if(Token::Separator(','));
-        lexer.expect_generic_paren('>')?;
+        lexer.expect(Token::TemplateArgsEnd)?;
         Ok((ty, span))
     }
 
@@ -1685,7 +1685,7 @@ impl Parser {
                 ast::Type::Atomic(scalar)
             }
             "ptr" => {
-                lexer.expect_generic_paren('<')?;
+                lexer.expect(Token::TemplateArgsStart)?;
                 let (ident, span) = lexer.next_ident_with_span()?;
                 let mut space = conv::map_address_space(ident, span, &lexer.enable_extensions)?;
                 lexer.expect(Token::Separator(','))?;
@@ -1699,11 +1699,11 @@ impl Parser {
                         crate::StorageAccess::LOAD
                     };
                 }
-                lexer.expect_generic_paren('>')?;
+                lexer.expect(Token::TemplateArgsEnd)?;
                 ast::Type::Pointer { base, space }
             }
             "array" => {
-                lexer.expect_generic_paren('<')?;
+                lexer.expect(Token::TemplateArgsStart)?;
                 let base = self.type_specifier(lexer, ctx)?;
                 let size = if lexer.end_of_generic_arguments() {
                     let size = self.const_generic_expression(lexer, ctx)?;
@@ -1712,12 +1712,12 @@ impl Parser {
                 } else {
                     ast::ArraySize::Dynamic
                 };
-                lexer.expect_generic_paren('>')?;
+                lexer.expect(Token::TemplateArgsEnd)?;
 
                 ast::Type::Array { base, size }
             }
             "binding_array" => {
-                lexer.expect_generic_paren('<')?;
+                lexer.expect(Token::TemplateArgsStart)?;
                 let base = self.type_specifier(lexer, ctx)?;
                 let size = if lexer.end_of_generic_arguments() {
                     let size = self.unary_expression(lexer, ctx)?;
@@ -1726,7 +1726,7 @@ impl Parser {
                 } else {
                     ast::ArraySize::Dynamic
                 };
-                lexer.expect_generic_paren('>')?;
+                lexer.expect(Token::TemplateArgsEnd)?;
 
                 ast::Type::BindingArray { base, size }
             }
