ae9a7eb7cfb5959f2b01648c067d7e5a4c131bbd
Jim Blandy <jimb@red-bean.com>
2026-01-06 15:10:37 -0800

    [naga wgsl-in] Rename `component_or_swizzle_specifier` arg.
    In `naga::front::wgsl::parse::Parser::component_or_swizzle_specifier`,
    since `span_start` sounds like the start of a span, rename that
    argument to `expr_start`, since it is the span of the start of the
    expression that ends with the component or swizzle specifier.

--- naga/src/front/wgsl/parse/mod.rs
+++ naga/src/front/wgsl/parse/mod.rs
@@ -532,7 +532,7 @@ impl Parser {
 
     fn component_or_swizzle_specifier<'a>(
         &mut self,
-        span_start: Span,
+        expr_start: Span,
         lexer: &mut Lexer<'a>,
         ctx: &mut ExpressionContext<'a, '_, '_>,
         expr: Handle<ast::Expression<'a>>,
@@ -557,7 +557,7 @@ impl Parser {
                 _ => break,
             };
 
-            let span = lexer.span_with_start(span_start);
+            let span = lexer.span_with_start(expr_start);
             expr = ctx.expressions.append(expression, span);
         }
 
