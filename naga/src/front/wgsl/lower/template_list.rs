880be33a6593109144919460f3707107ba90a933
teoxoy <28601907+teoxoy@users.noreply.github.com>
2026-01-15 14:53:36 +0100

    [wgsl-in] move `enumerant` & `var_address_space` functions on `ExpressionContext`

--- naga/src/front/wgsl/lower/template_list.rs
+++ naga/src/front/wgsl/lower/template_list.rs
@@ -94,7 +94,7 @@ impl<'iter, 'source> TemplateListIter<'iter, 'source> {
         ctx: &ExpressionContext<'source, '_, '_>,
     ) -> Result<'source, ir::AddressSpace> {
         let expr = self.expect_next("`AS`, an address space")?;
-        let (enumerant, span) = Lowerer::enumerant(expr, ctx)?;
+        let (enumerant, span) = ctx.enumerant(expr)?;
         conv::map_address_space(enumerant, span, &ctx.enable_extensions)
     }
     pub fn maybe_address_space(
@@ -102,7 +102,7 @@ impl<'iter, 'source> TemplateListIter<'iter, 'source> {
         ctx: &ExpressionContext<'source, '_, '_>,
     ) -> Result<'source, Option<ir::AddressSpace>> {
         if let Some(expr) = self.template_list.next() {
-            let (enumerant, span) = Lowerer::enumerant(*expr, ctx)?;
+            let (enumerant, span) = ctx.enumerant(*expr)?;
             Ok(Some(conv::map_address_space(
                 enumerant,
                 span,
@@ -118,7 +118,7 @@ impl<'iter, 'source> TemplateListIter<'iter, 'source> {
         ctx: &ExpressionContext<'source, '_, '_>,
     ) -> Result<'source, ir::StorageAccess> {
         let expr = self.expect_next("`Access`, an access mode")?;
-        let (enumerant, span) = Lowerer::enumerant(expr, ctx)?;
+        let (enumerant, span) = ctx.enumerant(expr)?;
         conv::map_access_mode(enumerant, span)
     }
     pub fn maybe_access_mode(
@@ -128,7 +128,7 @@ impl<'iter, 'source> TemplateListIter<'iter, 'source> {
     ) -> Result<'source, ()> {
         if let &mut ir::AddressSpace::Storage { ref mut access } = space {
             if let Some(expr) = self.template_list.next() {
-                let (enumerant, span) = Lowerer::enumerant(*expr, ctx)?;
+                let (enumerant, span) = ctx.enumerant(*expr)?;
                 let access_mode = conv::map_access_mode(enumerant, span)?;
                 *access = access_mode;
             } else {
@@ -144,7 +144,7 @@ impl<'iter, 'source> TemplateListIter<'iter, 'source> {
         ctx: &ExpressionContext<'source, '_, '_>,
     ) -> Result<'source, ir::StorageFormat> {
         let expr = self.expect_next("`Format`, a texel format")?;
-        let (enumerant, span) = Lowerer::enumerant(expr, ctx)?;
+        let (enumerant, span) = ctx.enumerant(expr)?;
         conv::map_storage_format(enumerant, span)
     }
 
@@ -153,7 +153,7 @@ impl<'iter, 'source> TemplateListIter<'iter, 'source> {
         ctx: &ExpressionContext<'source, '_, '_>,
     ) -> Result<'source, bool> {
         if let Some(expr) = self.template_list.next() {
-            let (enumerant, span) = Lowerer::enumerant(*expr, ctx)?;
+            let (enumerant, span) = ctx.enumerant(*expr)?;
             conv::map_ray_flag(&ctx.enable_extensions, enumerant, span)?;
             Ok(true)
         } else {
